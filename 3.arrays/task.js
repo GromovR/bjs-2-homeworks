25fc77add0e18c4eb8686691cb65bb3aabe0cfc2
GromovR <139163797+GromovR@users.noreply.github.com>
2024-01-08 11:58:58 +0300

    correct

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -8,7 +8,7 @@ function compareArrays(arr1, arr2) {
 
 
 
-function getUserNamesInAgeRange1(users, gender) {
+function getUserNamesInAgeRange(users, gender) {
   return users.filter(user => user.gender === gender)
 .map(user => user.age)
 .reduce((acc, age, index, arr) => acc + age / arr.length, 0);
